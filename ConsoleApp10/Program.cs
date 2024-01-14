2a14e7ff7e0c230bb023d438458389dede86d291
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:53:56 +0530

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace patterns
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write( j );
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
